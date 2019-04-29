bb5e9ffa9f2739c8b2f6288d9c3268ee4115b292
danieldayton33 <danieldayton33@gmailcom>
2019-04-29 17:04:18 -0400

    name change

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,31 @@
 import React from 'react';
-
-
+import Container from './Container';
+import Row from './Row';
+import Col from './Col';
 
 const NavBar = (props) => {
     return (
         <nav className="navbar navbar-expand-xl sticky-top navbar-light bg-dark">
             <div className="score-area">Score: {props.score}</div>
             <div className="logo" >
-                <img src="./images/b7c243ffdd6bd65c0fe0f6fde12848d1.png" style={{height:50, width: 700}}alt="logo" />
+                <img src="./images/e17d761fb238051d8f362859373756a3.png" style={{height:50, width: 700}}alt="logo" />
             </div>
-            <div className="high-score-area">High Score: {props.highScore}</div>
-            <div className="status">
-             {(props.mistakeMade === false) ? ("Guess. No Matches!") : ("Wrong! Try Again!")}
-            </div> 
+            <Container>
+                <Row>
+                <Col size={"sm-12"}>
+                <div className="high-score-area">High Score: {props.highScore}</div>
+                </Col>
+                </Row>
+                <Row>
+                <Col size={"sm-12"}>
+                <div className="status">
+                {(props.mistakeMade === false) ? ("Guess. No Matches!") : ("Wrong! Try Again!")}
+                </div> 
+                </Col>
+                
+                </Row>
+            </Container>
+            
         </nav>
     );
 }
