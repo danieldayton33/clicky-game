3bf4c171e255ce30d5e55851e9048958646c7354
danieldayton33 <danieldayton33@gmailcom>
2019-04-29 22:34:24 -0400

    animation for wrong answer

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,24 +3,25 @@ import Container from './Container';
 import Row from './Row';
 import Col from './Col';
 
-const NavBar = (props) => {
+const NavBar = ({score, mistakeMade, highScore}) => {
     return (
         <nav className="navbar navbar-expand-xl sticky-top navbar-light bg-dark">
-            <div className="score-area">Score: {props.score}</div>
+            <div className={(mistakeMade === false) ? ("score-area") : ("score-area shake")}>Score: {score}</div>
             <div className="logo" >
                 <img src="./images/e17d761fb238051d8f362859373756a3.png" style={{height:50, width: 700}}alt="logo" />
             </div>
             <Container>
                 <Row>
                 <Col size={"sm-12"}>
-                <div className="high-score-area">High Score: {props.highScore}</div>
+                <div className="high-score-area">High Score: {highScore}</div>
                 </Col>
                 </Row>
                 <Row>
                 <Col size={"sm-12"}>
-                <div className="status">
-                {(props.mistakeMade === false) ? ("Guess. No Matches!") : ("Wrong! Try Again!")}
-                </div> 
+                {(mistakeMade === false) ? 
+                   ( <div className="status">Guess. No Matches!</div> ) 
+                   :
+                    (<div className="status shake">Wrong! Try Again!</div> )}
                 </Col>
                 
                 </Row>
