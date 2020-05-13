d47f09af1ca6f531e3924af2bd26d454f5a6cb3a
danieldcarimus <danield@carimus.com>
2020-05-13 07:48:12 -0400

    style changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,8 @@ import React from 'react';
 
 function Card(props) {
     return (
-        <div className={props.shake === false ? (" image-div ") : (" image-div shake")} style={{width: "18rem"}}>
-            <img src={props.src} className="card-img-top" id={props.id} alt="pic-card" 
-            style={{borderRadius: "50%"}} 
+        <div className={props.shake === false ? (" image-div ") : (" image-div shake")}>
+            <img src={props.src} className="card-img-top" id={props.id} alt="pic-card"
             onClick={()=>props.handleClick(props.id, props.index)} />
            
         </div>
